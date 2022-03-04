85ef25ddbe8ecdcd010cc05ebecab339a9bd544d
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-04 12:10:05 +0100

    doc: Added documentation to Fibonacci

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,9 +1,23 @@
 import java.math.BigInteger;
 
+/**
+ * A static class used to compute Fibonacci numbers
+ */
 public class Fibonacci {
 
     private Fibonacci(){}
 
+    /**
+     * Computes the Fibonacci sequence.
+     * <p>
+     * Given a valid position, this method will compute the nth number on the
+     * Fibonacci sequence, starting from 0. Null arguments will produce a null
+     * output. This implementation uses a non-recursive algorithm to prevent
+     * stack overflows
+     *
+     * @param position A BigInteger representing the position on the Fibonacci sequence, starting from 0
+     * @return The computed Fibonacci number
+     */
     public static BigInteger fibonacci(BigInteger position) {
         if (position == null) return null;
 
