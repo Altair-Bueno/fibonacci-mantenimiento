cc3a396e142b79a2075d6087331dc68405464dc7
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-03-04 12:00:38 +0100

    new: Fibonacci.java

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,20 @@
+import java.math.BigInteger;
+
+public class Fibonacci {
+    public static BigInteger fibonacci(BigInteger position) {
+        if (position == null) return null;
+
+        var nth = BigInteger.ZERO;
+        var nthPlusOne = BigInteger.ONE;
+
+        while (!position.equals(BigInteger.ZERO)) {
+            var temp = nth;
+            nth = nthPlusOne;
+            nthPlusOne = temp.add(nthPlusOne);
+            // Note: BigIntegers are Inmutable, and thus this will not affect
+            // the caller
+            position = position.subtract(BigInteger.ONE);
+        }
+        return nth;
+    }
+}
